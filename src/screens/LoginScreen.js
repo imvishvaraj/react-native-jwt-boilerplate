93a6e3f7d21a59d457f849574363bfab7f9e7081
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2023-11-05 00:37:43 +0530

    updates

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, Text, StyleSheet, Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const login = async () => {
+    try {
+      let response = await fetch("http://localhost:8000/users/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      let json = await response.json();
+
+      if (response.ok) {
+        // Assuming the token is in the json response and navigation is set up
+        const access_token = json.tokens.access;
+
+        if (access_token) {
+          // Convert the token to a string if it's not already
+          const tokenString = String(access_token);
+          await AsyncStorage.setItem("access_token", tokenString);
+          navigation.navigate("UserInfo");
+        } else {
+          // Handle the absence of a token in the response
+          throw new Error("No token received");
+        }
+      } else {
+        // If the response status code is not OK, handle it as an error
+        throw new Error(json.message || "An error occurred during login.");
+      }
+    } catch (error) {
+      // If an exception is caught, set it as the error message to be displayed
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {errorMessage ? (
+        <Text style={styles.errorText}>{errorMessage}</Text>
+      ) : null}
+      <Button title="Login" onPress={login} />
+      <Button
+        title="Register"
+        onPress={() => navigation.navigate('Register')} // Make sure 'Register' matches the name in the navigator
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    fontSize: 16,
+    borderBottomColor: "gray",
+  },
+  errorText: {
+    color: "red",
+    marginBottom: 20,
+  },
+});
+
+export default LoginScreen;
---
+++ src/screens/UserInfoScreen.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
+const UserInfoScreen = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      setIsLoading(true);
+      try {
+        const access_token = await AsyncStorage.getItem('access_token');
+        const response = await fetch('http://localhost:8000/users/me/', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${access_token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        const json = await response.json();
+        if (response.ok) {
+          setUserInfo(json);
+        } else {
+          throw new Error(json.message || 'Error fetching user info');
+        }
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getUserInfo();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.error}>{error}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>User Information</Text>
+      {userInfo && (
+        <View>
+          <Text style={styles.info}>Name: {userInfo.name}</Text>
+          <Text style={styles.info}>Email: {userInfo.email}</Text>
+          {/* Display other user info as needed */}
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  info: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  error: {
+    color: 'red',
+  },
+});
+
+export default UserInfoScreen;
